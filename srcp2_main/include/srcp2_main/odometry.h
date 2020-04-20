9065d92bc3b8d1486a25391521fc1fbe78796016
Kevin French <fnivek@gmail.com>
2020-04-19 20:48:09 -0400

    ODOMETRY: Start error state Kalman filter on IMU, Test simple low pass filter on IMU data, Add TF broadcaster from IMU to world

--- srcp2_main/include/srcp2_main/odometry.h
+++ srcp2_main/include/srcp2_main/odometry.h
@@ -4,13 +4,20 @@
 // Ros
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_eigen/tf2_eigen.h>
 // Ros msgs
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/Pose.h>
-#include <geometry_msgs/Twist.h>
-#include <geometry_msgs/Wrench.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Transform.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <std_msgs/Header.h>
+
+// Eigen
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
 
 // C++
 #include <string>
@@ -18,6 +25,26 @@
 #include <queue>
 #include <mutex>
 #include <iostream>
+#include <math.h>
+#include <iostream>
+
+#define SAMPLEFILTER_TAP_NUM 15
+
+typedef struct
+{
+    double history[SAMPLEFILTER_TAP_NUM];
+    unsigned int last_index;
+} SampleFilter;
+
+void SampleFilter_init(SampleFilter* f);
+void SampleFilter_put(SampleFilter* f, double input);
+double SampleFilter_get(SampleFilter* f);
+
+static double filter_taps[SAMPLEFILTER_TAP_NUM] = {
+    -0.008268912593598236, -0.012977645844522306, 0.027099288599285735, 0.024009121542281034,  -0.07849418531704068,
+    -0.035125416972925406, 0.30807679910566016,   0.5394467059932627,   0.30807679910566016,   -0.035125416972925406,
+    -0.07849418531704068,  0.024009121542281034,  0.027099288599285735, -0.012977645844522306, -0.008268912593598236
+};
 
 class Odometry
 {
@@ -48,14 +75,25 @@ class Odometry
     // TF
     std::unique_ptr<tf2_ros::Buffer> tf_buf_;
     std::unique_ptr<tf2_ros::TransformListener> tf_listener_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     // Incoming msg queues
     std::mutex imu_lock_;
     std::queue<sensor_msgs::Imu> imu_queue_;
     // Current state
-    std_msgs::Header cur_header_;
-    geometry_msgs::Pose cur_pose_;
-    geometry_msgs::Twist cur_twist_;
-    geometry_msgs::Wrench cur_wrench_;
+    float last_time_;
+    Eigen::Vector3f pos_;
+    Eigen::Vector3f vel_;
+    Eigen::Quaternionf head_;
+    Eigen::Vector3f accel_bias_;
+    Eigen::Vector3f ang_vel_bias_;
+    Eigen::Vector3f grav_;
+    Eigen::Matrix<float, 18, 18> err_cov_;
+
+    // Debug
+    ros::Publisher debug_pub_;
+    SampleFilter debug_filt_x_;
+    SampleFilter debug_filt_y_;
+    SampleFilter debug_filt_z_;
 };
 
 #endif
--- srcp2_main/src/odometry.cpp
+++ srcp2_main/src/odometry.cpp
@@ -1,12 +1,52 @@
 #include "srcp2_main/odometry.h"
 
+void SampleFilter_init(SampleFilter* f)
+{
+    int i;
+    for (i = 0; i < SAMPLEFILTER_TAP_NUM; ++i)
+        f->history[i] = 0;
+    f->last_index = 0;
+}
+
+void SampleFilter_put(SampleFilter* f, double input)
+{
+    f->history[f->last_index++] = input;
+    if (f->last_index == SAMPLEFILTER_TAP_NUM)
+        f->last_index = 0;
+}
+
+double SampleFilter_get(SampleFilter* f)
+{
+    double acc = 0;
+    int index = f->last_index, i;
+    for (i = 0; i < SAMPLEFILTER_TAP_NUM; ++i)
+    {
+        index = index != 0 ? index - 1 : SAMPLEFILTER_TAP_NUM - 1;
+        acc += f->history[index] * filter_taps[i];
+    };
+    return acc;
+}
+
 /**
  * @brief      Constructs a new instance.
  *
  * @param[in]  robot_name  The robot name
  */
-Odometry::Odometry(std::string robot_name) : robot_name_(robot_name)
+Odometry::Odometry(std::string robot_name)
+  : robot_name_(robot_name)
+  , pos_(0, 0, 0)
+  , vel_(0, 0, 0)
+  , head_(1, 0, 0, 0)
+  , accel_bias_(0, 0, 0)
+  , ang_vel_bias_(0, 0, 0)
+  , grav_(0, 0, 1.62)
+  , last_time_(0)
 {
+    err_cov_ = Eigen::Matrix<float, 18, 18>::Zero();
+    // Debug filters
+    SampleFilter_init(&debug_filt_x_);
+    SampleFilter_init(&debug_filt_y_);
+    SampleFilter_init(&debug_filt_z_);
 }
 
 /**
@@ -18,15 +58,18 @@ Odometry::Odometry(std::string robot_name) : robot_name_(robot_name)
  */
 bool Odometry::setup()
 {
-    // Connect to ros topics
     ros::NodeHandle nh;
+    // Make publisher
+    debug_pub_ = nh.advertise<geometry_msgs::PoseStamped>("odom_debug", 1);
+    // Start tf
+    tf_broadcaster_.reset(new tf2_ros::TransformBroadcaster);
+    tf_buf_.reset(new tf2_ros::Buffer);
+    tf_listener_.reset(new tf2_ros::TransformListener(*tf_buf_));
+    // Connect to ros topics
     imu_sub_.reset(new ros::Subscriber);
     *imu_sub_ = nh.subscribe("/" + robot_name_ + "/imu", 10, &Odometry::imuCallback, this);
     joint_states_sub_.reset(new ros::Subscriber);
     *joint_states_sub_ = nh.subscribe("/" + robot_name_ + "/joint_states", 10, &Odometry::jointStatesCallback, this);
-    // Start tf listener
-    tf_buf_.reset(new tf2_ros::Buffer);
-    tf_listener_.reset(new tf2_ros::TransformListener(*tf_buf_));
 }
 
 void Odometry::update()
@@ -38,11 +81,27 @@ void Odometry::update()
         sensor_msgs::Imu msg;
         {
             std::lock_guard<std::mutex> lock(imu_lock_);
-            msg = imu_queue_.back();
+            msg = imu_queue_.front();
         }
 
         // Do math
         imuUpdate(msg);
+
+        // Update tf
+        geometry_msgs::TransformStamped tf_msg;
+        Eigen::Affine3d tf;
+        tf.linear() = head_.cast<double>().toRotationMatrix();  // Maybe change to message heading
+        tf.translation() = pos_.cast<double>();
+        tf = tf.inverse();
+        geometry_msgs::Pose pose = tf2::toMsg(tf);
+        tf_msg.transform.translation.x = pose.position.x;
+        tf_msg.transform.translation.y = pose.position.y;
+        tf_msg.transform.translation.z = pose.position.z;
+        tf_msg.transform.rotation = pose.orientation;
+        tf_msg.header.stamp = msg.header.stamp;
+        tf_msg.header.frame_id = msg.header.frame_id;
+        tf_msg.child_frame_id = "/world";
+        tf_broadcaster_->sendTransform(tf_msg);
 
         // Remove last element
         {
@@ -59,38 +118,128 @@ void Odometry::update()
  */
 void Odometry::imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
+    std::lock_guard<std::mutex> lock(imu_lock_);
     imu_queue_.emplace(*msg);
 }
 
 void Odometry::imuUpdate(const sensor_msgs::Imu& msg)
 {
-    // Simple constant acceleration motion model
-    float dt = (float)(msg.header.stamp - cur_header_.stamp).toSec();
-    cur_pose_.position.x += 0.5 * cur_wrench_.force.x * cur_wrench_.force.x * dt + cur_twist_.linear.x * dt;
-    cur_pose_.position.y += 0.5 * cur_wrench_.force.y * cur_wrench_.force.y * dt + cur_twist_.linear.y * dt;
-    cur_pose_.position.z += 0.5 * cur_wrench_.force.z * cur_wrench_.force.z * dt + cur_twist_.linear.z * dt;
-    cur_twist_.linear.x += cur_wrench_.force.x * dt;
-    cur_twist_.linear.y += cur_wrench_.force.y * dt;
-    cur_twist_.linear.z += cur_wrench_.force.z * dt;
+    // Timing information
+    float dt = msg.header.stamp.toSec() - last_time_;
+    if (last_time_ == 0)
+    {
+        last_time_ = msg.header.stamp.toSec();
+        return;
+    }
+    if (dt <= 0)
+        return;
 
-    cur_header_ = msg.header;
+    // Convert message to Eigen
+    Eigen::Vector3f mes_accel(msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z);
+    mes_accel -= accel_bias_;
+    Eigen::Vector3f mes_vel(msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z);
+    mes_vel -= ang_vel_bias_;
+    Eigen::Quaternionf mes_head(msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z);
+    // Eigen::Matrix3f mes_rot(mes_head.toRotationMatrix());
+    Eigen::Matrix3f rot(mes_head.toRotationMatrix());
 
-    cur_twist_.angular.x = msg.angular_velocity.x;
-    cur_twist_.angular.y = msg.angular_velocity.y;
-    cur_twist_.angular.z = msg.angular_velocity.z;
+    SampleFilter_put(&debug_filt_x_, mes_accel[0]);
+    SampleFilter_put(&debug_filt_y_, mes_accel[1]);
+    SampleFilter_put(&debug_filt_z_, mes_accel[2]);
 
-    cur_pose_.orientation.x = msg.orientation.x;
-    cur_pose_.orientation.x = msg.orientation.y;
-    cur_pose_.orientation.x = msg.orientation.z;
-    cur_pose_.orientation.w = msg.orientation.w;
+    std::cout << "----------------------\n"
+              << "mes_accel:\n"
+              << mes_accel << std::endl
+              << "mes_vel:\n"
+              << mes_vel << std::endl
+              << "mes_head:\n"
+              << mes_head.toRotationMatrix() << std::endl
+              << "mes_head.angularDistance(Eigen::Quaternionf(Eigen::AngleAxisf(0, Eigen::Vector3f::UnitZ()))):\n"
+              << mes_head.angularDistance(Eigen::Quaternionf(Eigen::AngleAxisf(0, Eigen::Vector3f::UnitZ())))
+              << std::endl
+              << "rot:\n"
+              << rot << std::endl
+              << "diff:\n"
+              << mes_head.angularDistance(head_) << std::endl
+              << "mes_accel.norm():\n"
+              << mes_accel.norm() << std::endl
+              << "rot * mes_accel:\n"
+              << rot * mes_accel << std::endl
+              << "(rot * mes_accel - grav_):\n"
+              << (rot * mes_accel - grav_) << std::endl
+              << "(rot * mes_accel - grav_).norm():\n"
+              << (rot * mes_accel - grav_).norm() << std::endl
+              << "SampleFilter_get(&debug_filt_x_):\n"
+              << SampleFilter_get(&debug_filt_x_) << std::endl
+              << "SampleFilter_get(&debug_filt_y_):\n"
+              << SampleFilter_get(&debug_filt_y_) << std::endl
+              << "SampleFilter_get(&debug_filt_z_):\n"
+              << SampleFilter_get(&debug_filt_z_) << std::endl;
 
-    cur_wrench_.force.x = msg.linear_acceleration.x;
-    cur_wrench_.force.y = msg.linear_acceleration.y;
-    cur_wrench_.force.z = msg.linear_acceleration.z;
+    geometry_msgs::PoseStamped debug_head;
+    debug_head.header = msg.header;
+    debug_head.pose.position.x = 0;
+    debug_head.pose.position.y = 0;
+    debug_head.pose.position.z = 0;
+    debug_head.pose.orientation.x = mes_head.x();
+    debug_head.pose.orientation.y = mes_head.y();
+    debug_head.pose.orientation.z = mes_head.z();
+    debug_head.pose.orientation.w = mes_head.w();
+    debug_pub_.publish(debug_head);
 
-    std::cout << cur_pose_ << std::endl;
-    std::cout << cur_twist_ << std::endl;
-    std::cout << cur_wrench_ << std::endl;
+    // Nominal state update
+    last_time_ = msg.header.stamp.toSec();
+    std::cout << "dt: " << dt << std::endl;
+    pos_ += vel_ * dt + 0.5 * (rot * mes_accel - grav_) * dt * dt;
+    vel_ += (rot * mes_accel - grav_) * dt;
+    mes_vel *= dt;
+    float phi = mes_vel.norm();
+    Eigen::Vector3f u_sin_phi = std::sin(phi / 2) * mes_vel / phi;
+    head_ *= Eigen::Quaternionf(std::cos(phi / 2), u_sin_phi.x(), u_sin_phi.y(), u_sin_phi.z());
+    // accel_bias_ = accel_bias_;
+    // ang_vel_bias_ = ang_vel_bias_;
+    // grav_ = grav_;
+
+    // TODO(Kevin): Put this in a utils file
+    auto skew_symetric = [](const Eigen::Vector3f& v) {
+        Eigen::Matrix3f m = Eigen::Matrix3f::Zero();
+        m << 0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0;
+        return m;
+    };
+
+    // Calculate Jacobian
+    Eigen::Matrix<float, 18, 18> Fx = Eigen::Matrix<float, 18, 18>::Identity();
+    Fx.block<3, 3>(0, 3) = Eigen::Matrix3f::Identity() * dt;
+    Fx.block<3, 3>(3, 6) = -rot * skew_symetric(mes_accel) * dt;
+    Fx.block<3, 3>(3, 9) = -rot * dt;
+    Fx.block<3, 3>(3, 15) = Fx.block<3, 3>(0, 3);
+    // Fx.block<3, 3>(6, 6) = rot.transpose() * mes_vel * dt;  // TODO(Kevin): Figure out what the notation R{x}
+    // means
+    Fx.block<3, 3>(6, 12) = -Fx.block<3, 3>(0, 3);
+
+    Eigen::Matrix<float, 18, 12> Fi = Eigen::Matrix<float, 18, 12>::Zero();
+    Fi.block<12, 12>(3, 0) = Eigen::Matrix<float, 12, 12>::Identity();
+
+    // TODO(Kevin): Define this elsewhere and find real parameters
+    Eigen::Matrix<float, 12, 12> Q = Eigen::Matrix<float, 12, 12>::Identity() * 0.01;
+
+    // Error mean always 0
+    err_cov_ = Fx * err_cov_ * Fx.transpose() + Fi * Q * Fi.transpose();
+
+    std::cout << "----------------------\n"
+              << "pos_:\n"
+              << pos_ << std::endl
+              << "vel_:\n"
+              << vel_ << std::endl
+              << "head_:\n"
+              << head_.matrix() << std::endl
+              << "accel_bias_:\n"
+              << accel_bias_ << std::endl
+              << "ang_vel_bias_:\n"
+              << ang_vel_bias_ << std::endl
+              << "grav_:\n"
+              << grav_ << std::endl;
+    // << "err_cov_:\n" << err_cov_ << std::endl;
 }
 
 /**
