5820e415eb93cb969f8673516949a18dd313d26f
Kevin French <fnivek@gmail.com>
2020-06-05 23:27:28 -0400

    QUALS1: Make a quals 1 class and test mcl

--- srcp2_main/src/nodes/quals_1.cpp
+++ srcp2_main/src/nodes/quals_1.cpp
@@ -1,22 +1,56 @@
+// Stl
+#include <string>
+
+// Ros
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+// srcp2
 #include "srcp2_main/odometry.h"
 #include "srcp2_main/mcl.h"
+
+class Quals1
+{
+  public:
+    std::string robot_name_;
+    Mcl mcl_;
+    ros::Subscriber laser_sub_;
+    // Odometry odom_;
+
+  public:
+    Quals1() : robot_name_("scout_1"), mcl_(robot_name_)  //  , odom_("scout_1")
+    {
+        // Setup robot
+        // odom.setup();
+        Eigen::Affine3d zero(Eigen::Translation3d(0, 0, 0));
+        mcl_.setup(zero);
+
+        // Start subsribers
+        ros::NodeHandle nh;
+        laser_sub_ = nh.subscribe("/" + robot_name_ + "/laser/scan", 10, &Quals1::laserCallback, this);
+    }
+
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+    {
+        mcl_.update(*msg);
+    }
+
+    void run()
+    {
+        ros::Rate loop_rate(1000);
+        while (ros::ok())
+        {
+            loop_rate.sleep();
+            ros::spinOnce();
+        }
+    }
+};
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "srcp2_quals_1");
     ros::NodeHandle nh;
-    // Odometry odom("scout_1");
-    // odom.setup();
-    Mcl mcl("scout_1");
-    mcl.setup(Eigen::Affine3d());
 
-    ros::Rate loop_rate(10);
-    while (ros::ok())
-    {
-        // odom.update();
-        // mcl.update();
-        loop_rate.sleep();
-        ros::spinOnce();
-    }
+    Quals1 q1;
+    q1.run();
 }
