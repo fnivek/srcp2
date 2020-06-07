73915aec337927e134f20e7684f8abc3c00cca4e
Kevin French <fnivek@gmail.com>
2020-06-07 18:40:19 -0400

    QUALS1: Initialize mcl pose with gazebo or true pose service

--- srcp2_main/src/nodes/quals_1.cpp
+++ srcp2_main/src/nodes/quals_1.cpp
@@ -4,10 +4,12 @@
 // Ros
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <gazebo_msgs/GetModelState.h>
 
 // srcp2
 #include "srcp2_main/odometry.h"
 #include "srcp2_main/mcl.h"
+#include <srcp2_msgs/LocalizationSrv.h>
 
 class Quals1
 {
@@ -22,8 +24,27 @@ class Quals1
     {
         // Setup robot
         // odom.setup();
-        Eigen::Affine3d zero(Eigen::Translation3d(0, 0, 0));
-        mcl_.setup(zero);
+        geometry_msgs::Pose pose;
+        bool use_gazebo = true;
+        if (use_gazebo)
+        {
+            gazebo_msgs::GetModelState true_pose_srv;
+            true_pose_srv.request.model_name = robot_name_;
+            true_pose_srv.request.relative_entity_name = "lunar_terrain";
+            ros::service::call("/gazebo/get_model_state", true_pose_srv);
+            pose = true_pose_srv.response.pose;
+        }
+        else
+        {
+            srcp2_msgs::LocalizationSrv true_pose_srv;
+            ros::service::call("/" + robot_name_ + "/get_true_pose", true_pose_srv);
+            pose = true_pose_srv.response.pose;
+        }
+
+        Eigen::Affine3d true_pose =
+            Eigen::Translation3d(pose.position.x, pose.position.y, pose.position.z) *
+            Eigen::Quaterniond(pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z);
+        mcl_.setup(true_pose);
 
         // Start subsribers
         ros::NodeHandle nh;
